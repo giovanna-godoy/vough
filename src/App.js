a120cd3dc70a7128193cdef309a6fde225f5ecdf
Giovanna Godoy <giovannagodoy@icloud.com>
2021-07-01 23:33:41 -0300

    Updating file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import List from "./components/List";
-
+import Global from "./styles/global";
 
 class App extends Component {
   render() {
     return (
       <section>
+        <Global/>
         <List/>
       </section>
     );
